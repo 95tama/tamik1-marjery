2107c379c4957c481d17d31b8de85f2bc1cda406
tamerlanusenov <tamerlanusenov1@gmail.com>
2020-05-24 00:07:23 -0700

    Updates

---
+++ precache-manifest.1bd84cb977b781a0ee557528a15c8e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fff7c1b43e90a615e9a828a702f01e5",
+    "url": "/tamik1-marjery/index.html"
+  },
+  {
+    "revision": "a535592dfc84fe8e82a0",
+    "url": "/tamik1-marjery/static/css/main.04fbcc7c.chunk.css"
+  },
+  {
+    "revision": "5213642fa93828d9647e",
+    "url": "/tamik1-marjery/static/js/2.6b8cfce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tamik1-marjery/static/js/2.6b8cfce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a535592dfc84fe8e82a0",
+    "url": "/tamik1-marjery/static/js/main.da858056.chunk.js"
+  },
+  {
+    "revision": "3d029c57ff15d5ec34a7",
+    "url": "/tamik1-marjery/static/js/runtime-main.caffa6ea.js"
+  }
+]);
